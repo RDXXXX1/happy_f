8427f9edadd50ec6af5aa3b481ca553c851f6c52
RDXXXX1 <rdmishrax@gmail.com>
2024-08-31 17:55:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,43 @@
+// import React, { useEffect } from 'react';
+// import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+// import UserPanel from './components/UserPanel';
+// import ResponsePage from './components/ResponsePage';
+// import CustomNavbar from './components/Navbar';
+// import HomePage from './components/HomePage';
+// import './App.css';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <CustomNavbar className="navbar" />
+//       <AppRoutes />
+//     </Router>
+//   );
+// };
+
+// const AppRoutes = () => {
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     const params = new URLSearchParams(location.search);
+//     const token = params.get('token');
+
+//     if (token) {
+//       localStorage.setItem('authToken', token); // Save the token in localStorage
+//       window.history.replaceState({}, document.title, window.location.pathname); // Remove the token from the URL
+//     }
+//   }, [location]);
+
+//   return (
+//     <Routes>
+//       <Route path="/" element={<UserPanel />} />
+//       <Route path="/Home" element={<HomePage />} />
+//       <Route path="/response/:promptId" element={<ResponsePage />} />
+//     </Routes>
+//   );
+// };
+
+// export default App;
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import UserPanel from './components/UserPanel';
@@ -9,7 +49,6 @@ import './App.css';
 const App = () => {
   return (
     <Router>
-      <CustomNavbar className="navbar" />
       <AppRoutes />
     </Router>
   );
@@ -25,6 +64,7 @@ const AppRoutes = () => {
     if (token) {
       localStorage.setItem('authToken', token); // Save the token in localStorage
       window.history.replaceState({}, document.title, window.location.pathname); // Remove the token from the URL
+      window.location.reload(); // Refresh the page
     }
   }, [location]);
 
